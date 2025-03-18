1a4511ac8b95e0cc7d67adc3e90a741c4a4bde78
spicyshibe <rafierasyw@gmail.com>
2025-03-18 15:31:39 +0700

    perulangan for untuk pencacah naik fix

---
+++ file1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int i;
+    cout << "perulangan For Pecahan Naik" << endl;
+    for (i = 0; i <= 4; i++){
+        cout << "Nilai i : " << i 
+        << " Lari keliling lapangan " << endl;
+    }
+    cout << "nilai i terakhir : " << i << endl;
+    cout << endl;
+
+    cout << "perulangan for pencacahan turun" << endl;
+    for (i = 4; i >= 0; i--) {
+        
+    }
+
+}
